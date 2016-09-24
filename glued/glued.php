4061386449ca5fd446e034c4fbd579b98fed9634
killua-eu <pavel@vaizard.org>
2016-09-24 20:38:25 +0200

    Update glued.php

--- glued/glued.php
+++ glued/glued.php
@@ -25,6 +25,14 @@ print_r($user);
 $app = new \Slim\App([
     'settings' => [
         'displayErrorDetails' => true,
+        'mysql' => [,
+            'host' => '127.0.0.1',
+            'database' => 'slim-test',
+            'username' => 'slim',
+            'password' => '****',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode'
+        ]
     ]
 ]);
 
