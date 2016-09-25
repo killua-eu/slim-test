44956aae1b171d838eadc495ab3f4a0906df13b3
Pavel Stratil <pavel@vaizard.org>
2016-09-25 00:45:05 +0000

    adding example config

---
+++ glued/config.example.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true,
+        'mysql' => [
+            'host' => '127.0.0.1',
+            'database' => 'slim-test',
+            'username' => 'slim',
+            'password' => '****',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci'
+        ]
+    ]
+];
+
